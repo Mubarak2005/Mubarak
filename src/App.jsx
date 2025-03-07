f8d1dd156c95e74cd194a7b16d87cbe112572255
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-07 10:00:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import About from './Components/About';
+import Home from './Components/Home';
+import ProjectSection from './Components/ProjectSection';
+import BeforeAbout from './Components/BeforeAbout';
+import Contact from './Components/Contact';
+import Loading from './Components/Loading';
+import InfiniteScroller from './Components/InfiniteScroller';
+import AfterAbout from './Components/AfterAbout';
+import Footer from './Components/Footer';
+import Lenis from "@studio-freight/lenis";
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  
+  // useEffect(() => {
+  //   const lenis = new Lenis({
+  //     duration: 1.5, // Smoother scrolling experience
+  //     smooth: true,
+  //     smoothTouch: true, // Enables smooth scrolling on touch devices
+  //     easing: (t) => 1 - Math.pow(1 - t, 3), // Cubic easing for a natural feel
+  //   });
+
+  //   const update = (time) => {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(update);
+  //   };
+
+  //   requestAnimationFrame(update);
+
+  //   return () => {
+  //     lenis.destroy(); // Cleanup on unmount
+  //   };
+  // }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className='main h-full w-full text-[#fff]'>
+        {/* <Loading /> */}
+        <Home />
+        <BeforeAbout />
+        <InfiniteScroller />
+        <About />
+        <AfterAbout />
+        <ProjectSection />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
