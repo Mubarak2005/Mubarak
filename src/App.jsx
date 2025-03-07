b38a395e812ec6abfe6e4a504c27f8f81ed0eac7
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-07 11:38:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
     <div>
       <Navbar />
       <div className='main h-full w-full text-[#fff]'>
-        {/* <Loading /> */}
+        <Loading />
         <Home />
         <BeforeAbout />
         <InfiniteScroller />
