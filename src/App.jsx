047fb585d125ca4265d070bb018046c283ab0a11
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-13 16:46:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,41 @@ import Home from './Components/Home';
 import ProjectSection from './Components/ProjectSection';
 import BeforeAbout from './Components/BeforeAbout';
 import Contact from './Components/Contact';
-import Loading from './Components/Loading';
 import InfiniteScroller from './Components/InfiniteScroller';
-import AfterAbout from './Components/AfterAbout';
 import Footer from './Components/Footer';
 import Lenis from "@studio-freight/lenis";
 import Navbar from './Components/Navbar';
 
 const App = () => {
   
-  // useEffect(() => {
-  //   const lenis = new Lenis({
-  //     duration: 1.5, // Smoother scrolling experience
-  //     smooth: true,
-  //     smoothTouch: true, // Enables smooth scrolling on touch devices
-  //     easing: (t) => 1 - Math.pow(1 - t, 3), // Cubic easing for a natural feel
-  //   });
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.5, // Smoother scrolling experience
+      smooth: true,
+      smoothTouch: true, // Enables smooth scrolling on touch devices
+      easing: (t) => 1 - Math.pow(1 - t, 3), // Cubic easing for a natural feel
+    });
 
-  //   const update = (time) => {
-  //     lenis.raf(time);
-  //     requestAnimationFrame(update);
-  //   };
+    const update = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(update);
+    };
 
-  //   requestAnimationFrame(update);
+    requestAnimationFrame(update);
 
-  //   return () => {
-  //     lenis.destroy(); // Cleanup on unmount
-  //   };
-  // }, []);
+    return () => {
+      lenis.destroy(); // Cleanup on unmount
+    };
+  }, []);
 
   return (
     <div>
       <Navbar />
-      <div className='main h-full w-full text-[#fff]'>
-        <Loading />
+      <div className='main h-full w-full text-[#fff] mix-blend-difference'>
         <Home />
         <BeforeAbout />
         <InfiniteScroller />
         <About />
-        <AfterAbout />
         <ProjectSection />
         <Contact />
         <Footer />
