cc7bd24c6381880ea7aa2fb063a24ca95bcdcc05
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-14 23:30:31 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,17 @@ import gsap from 'gsap';
 import React, { useState, useEffect } from 'react';
 import { FaLinkedin, FaGithub, FaDiscord } from "react-icons/fa";
 import { MdArrowOutward } from "react-icons/md";
+import { Link } from 'react-scroll';
 
-const Navbar = () => {
+const Navbar = ({lenis}) => {
+
     const [navBar, setNavBar] = useState(false);
     
     useEffect(() => {
         const main = document.querySelector(".main");
         
         if (navBar) {
+            if (lenis) lenis.stop();
             gsap.to(".b1", { rotate: 45, position: "absolute" });
             gsap.to(".b2", { rotate: -45, position: "absolute" });
             gsap.to(".nav-section", {
@@ -39,13 +42,15 @@ const Navbar = () => {
                 }
             });
         } else {
-            document.body.style.overflow = "visible";
-            main.style.filter = "blur(0px)";
-            main.style.pointerEvents = "all";
-            main.style.opacity = "1";
-            gsap.to(".b1", { rotate: 0, position: "relative" });
-            gsap.to(".b2", { rotate: 0, position: "relative" });
-            gsap.to(".nav-section", {
+            const closeNavbar = ()=>{
+                if (lenis) lenis.start();
+                document.body.style.overflow = "visible";
+                main.style.filter = "blur(0px)";
+                main.style.pointerEvents = "all";
+                main.style.opacity = "1";
+                gsap.to(".b1", { rotate: 0, position: "relative" });
+                gsap.to(".b2", { rotate: 0, position: "relative" });
+                gsap.to(".nav-section", {
                 width: "0vw",
                 duration: 1,
                 borderTopLeftRadius: "50%",
@@ -55,9 +60,11 @@ const Navbar = () => {
                     gsap.to(".nav-el", { scale: 0, opacity: 0 });
                 }
             });
+            }
+            closeNavbar()
         }
         gsap.to(".navbar-l", {
-            delay: .5,
+            delay: 6.4,
             opacity: 1,
             filter: "blur(0px)"
         });
@@ -71,7 +78,7 @@ const Navbar = () => {
         <>
             <div className={`navbar-l opacity-0 blur-[10px] fixed font-[Azonix] text-[#fff] z-[991] top-5 w-full flex justify-between items-center px-3 md:px-10 mix-blend-difference`}>
                 <div>
-                    <img className='w-[7vh]' src="/images/mk_logo.webp" alt="Logo" loading="lazy" />
+                    <a href="https://mubarak-ansari.vercel.app/"><img className='w-[7vh]' src="/images/mk_logo.webp" alt="Logo" loading="lazy" /></a>
                 </div>
                 <div onClick={clickBurger} className="burger pointer-events-auto h-5 w-12 flex flex-col hover:flex-col-reverse justify-center items-center gap-2 hover:gap-3 ease-in-out transition-all duration-300 cursor-pointer relative">
                     <div className={`b1 w-10 md:w-12 h-[.1rem] bg-[#f0f0fdb6] relative`}></div>
@@ -90,26 +97,29 @@ const Navbar = () => {
                     </h1>
                     
                     <div className='w-full h-full text-[8vw] sm:text-[6vw] md:text-[3.5vw] xl:text-[2.5vw] flex justify-center items-start flex-col'>
-                        {['Home', 'About', 'Projects', 'Contact Me'].map((item, index) => (
-                            <div key={index} className='w-full border-b-[1px] border-[#000] relative overflow-hidden'>
-                                <h1 className='nav-h1 w-fit md:px-5 relative group hover:cursor-pointer flex items-center md:gap-2'>
-                                    <MdArrowOutward className='hidden xl:block md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300' />
-                                    {item}
-                                    <span className='text-[10vw] md:text-[4vw] opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-[Abuget] gradient-anim2 duration-500 group-hover:opacity-100'>{item}</span>
-                                </h1>
-                            </div>
-                        ))}
+                        {['Home', 'About', 'Projects', 'Contact Me'].map((item, index) => {
+                            const sectionId = item.toLowerCase().replace(/\s+/g, "-"); // Convert to lowercase & replace spaces
+                            return (
+                                <div key={index} className='w-full border-b-[1px] border-[#000] relative overflow-hidden'>
+                                    <h1 className='nav-h1 w-fit md:px-5 relative group hover:cursor-pointer flex items-center md:gap-2'>
+                                        <Link className='flex items-center' to={sectionId} smooth={false} duration={500} spy={true} offset={-70} onClick={() => setNavBar(false)} >
+                                        <MdArrowOutward className='hidden xl:block md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300' />{item}</Link>
+                                        <span className='text-[10vw] md:text-[4vw] pointer-events-none opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-[Abuget] gradient-anim2 duration-500 group-hover:opacity-100'>{item}</span>
+                                    </h1>
+                                </div>
+                            );
+                        })}
                     </div>
-                    <div className='w-full h-full text-[10vw] md:text-[3vw] flex justify-center gap-20'>
-                        <div className='flex justify-center items-center'>
-                            <FaLinkedin className='cursor-pointer hover:text-[#0073B1] duration-200 transition-all' />
-                        </div>
-                        <div className='flex justify-center items-center'>
-                            <FaGithub className='cursor-pointer duration-200 transition-all' />
-                        </div>
-                        <div className='flex justify-center items-center'>
-                            <FaDiscord className='cursor-pointer hover:text-[#5562EA] duration-200 transition-all' />
-                        </div>
+                    <div className='w-full h-full text-[10vw] md:text-[2vw] flex justify-center items-center gap-20'>
+                        <a className='p-4 border-[.5px] border-[#000000b0] rounded-full ' href="https://www.linkedin.com/in/mubarak-ansari-4a6312264?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin className='cursor-pointer' />
+                        </a>
+                        <a className='p-4 border-[.5px] border-[#000000b0] rounded-full '  href="https://github.com/Mubarak2005" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className='cursor-pointer' />
+                        </a>
+                        <a className='p-4 border-[.5px] border-[#000000b0] rounded-full '  href="https://discord.com/channels/mubarak0653" target="_blank" rel="noopener noreferrer">
+                            <FaDiscord className='cursor-pointer' />
+                        </a>
                     </div>
                 </div>
             </div>
