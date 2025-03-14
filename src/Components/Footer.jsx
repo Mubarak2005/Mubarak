3420c5dab8e8f4703e692ad15baa2b59cce0883f
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-14 23:33:22 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MdArrowOutward } from 'react-icons/md'
+import { Link } from 'react-scroll'
 
 const Footer = () => {
   return (
@@ -11,12 +13,18 @@ const Footer = () => {
           {/* <h1 className='text-[5vw] leading-none gradient-anim2 font-[Abuget] px-4'>Mubarak Ansari</h1> */}
           <h1 className='text-[10vw] md:text-[5vw] leading-[1vw] font-[Brigends] text-center '><span className='font-[Abuget] px-3 text-[15vw] md:text-[10vw] gradient-anim leading-none relative z-[1] '>Mubarak</span> Ansari</h1>
         </div>
-        <div className='w-full md:w-1/2 h-1/2 md:h-full text-[5vw] md:text-[2.3vw] flex flex-row md:flex-col justify-between md:justify-start items-center md:gap-5 border-t-[1px] md:border-l-[1px] md:border-t-[0px] border-[#9d9dea5e] '>
-          <h1 className='w-full text-[7vw] md:text-[2.5vw] mt-5 leading-none text-center'>Quick Links</h1>
+        <div className='w-full md:w-1/2 h-1/2 md:h-full text-[5vw] md:text-[2vw] flex flex-row md:flex-col justify-between md:justify-start items-center md:gap-5 border-t-[1px] md:border-l-[1px] md:border-t-[0px] border-[#9d9dea5e] '>
+          <h1 className='w-full text-[7vw] md:text-[2.6vw] mt-5 leading-none text-center'>Quick Links</h1>
           <div className='w-full flex flex-col justify-center items-center mt-5'>
-            <h2>Home</h2>
-            <h2>About</h2>
-            <h2>Projects</h2>
+            <h2 className='cursor-pointer hover:underline'>
+              <Link className='flex items-center' to="home" smooth={false} duration={500} spy={true} offset={-70}>Home</Link>
+            </h2>
+            <h2 className='cursor-pointer hover:underline'>
+              <Link className='flex items-center' to="about" smooth={false} duration={500} spy={true} offset={-70}>About</Link>
+            </h2>
+            <h2 className='cursor-pointer hover:underline'>
+              <Link className='flex items-center' to="projects" smooth={false} duration={500} spy={true} offset={-70}>Projects</Link>
+            </h2>
           </div>
         </div>
       </div>
