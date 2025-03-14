a47289be3b181ec087ad2c3e0b2d6f65f1f094c8
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-15 00:05:04 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
-import { Element } from 'react-scroll'
 
 
 const Home = () => {
   return (
-    <Element name="home">
-        <section className="h-screen flex justify-center items-center">
+    <div name="home">
+        <div className="h-screen flex justify-center items-center">
           <Hero />
-        </section>
-      </Element>
+        </div>
+      </div>
   )
 }
 
