adcefa70cafc35a22db6a76f782a480e4c39ef8e
Mubarak2005 <132432485+Mubarak2005@users.noreply.github.com>
2025-03-15 12:39:41 +0530

    Update ProjectText.jsx

--- src/Components/ProjectText.jsx
+++ src/Components/ProjectText.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ProjectText = () => {
   return (
-    <div className='flex flex-row justify-center items-center gap-3 md:gap-5 px-2 md:px-10 mt-10'>
+    <div className='flex flex-row justify-center items-center gap-3 md:gap-5 px-2 md:px-10 mt-20'>
       <div className='w-full h-[.5px] bg-[#9d9dea5e]'></div>
         <div className='flex relative'>
-            <h1 className='text-[6vw] md:text-[4vw] font-[Brigends]'>Projects</h1>
-            <h1 className='text-[10vw] md:text-[7vw] font-[Abuget] gradient-anim absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Projects</h1>
+            <h1 className='text-[9vw] md:text-[4vw] font-[Brigends]'>Projects</h1>
+            <h1 className='text-[20vw] md:text-[8vw] font-[Abuget] text-[#ef3750] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Projects</h1>
         </div>
         
     </div>
